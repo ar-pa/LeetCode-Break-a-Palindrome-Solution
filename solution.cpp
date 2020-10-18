39de2d9013d2eec35edaa350f8c11bbed6a5c37f
amirreza poorakhavan <a.poorakhavan@gmail.com>
2020-10-18 13:29:57 +0330

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+  		if(palindrome.size() == 1)
+  			return "";
+  		for(int i = 0; i < palindrome.size() / 2; i++)
+  			if(palindrome[i] != 'a'){
+  				palindrome[i] = 'a'; 
+  				return palindrome;     
+  			}
+  		palindrome.back() = 'b';
+  		return palindrome;
+    }
+};
